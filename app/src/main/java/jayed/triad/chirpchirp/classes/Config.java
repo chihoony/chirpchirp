292ac1890e9cc56b4394b759adebef084e85f78d
msroh94 <msroh94@gmail.com>
2016-05-05 18:48:13 -0700

    now able to upload images to AWS S3 with hardcoded path

--- app/src/main/java/jayed/triad/chirpchirp/classes/Config.java
+++ app/src/main/java/jayed/triad/chirpchirp/classes/Config.java
@@ -5,12 +5,24 @@ package jayed.triad.chirpchirp.classes;
  */
 public class Config {
     private static String keyValue;
+    private static String accessKey = "AKIAI46EHF27JPKWY5FA";
+    private static String secretKey = "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";
 
     public Config() {
         this.keyValue = "us-east-1:53f7e3e8-345c-4913-8155-4640b53777cf";
+//        accessKey = "AKIAI46EHF27JPKWY5FA";
+//        secretKey = "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";
     }
 
     public String getKeyValue() {
         return this.keyValue;
     }
+    public static String getAccessKey() {
+        return accessKey;
+    }
+    public static String getSecretKey() {
+        return secretKey;
+    }
+
+
 }

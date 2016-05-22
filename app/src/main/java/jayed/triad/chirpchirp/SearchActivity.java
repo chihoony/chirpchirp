be88f4a6acb9e2ea4d4946941f2352845b1e16e0
jimgtchou <jimgtchou@gmail.com>
2016-05-22 13:55:56 -0700

    created search xml files

---
+++ app/src/main/java/jayed/triad/chirpchirp/SearchActivity.java
@@ -0,0 +1,18 @@
+package jayed.triad.chirpchirp;
+
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
+
+/**
+ * Created by jimmychou516 on 16-05-22.
+ */
+public class SearchActivity extends AppCompatPreferenceActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
+    }
+}
